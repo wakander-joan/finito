cad7e63ae3a8d6b5abd7becde84bbf912d102f59
joan.santos <joansantosreis@gmail.com>
2025-08-26 02:01:11 -0300

    Ação nos Meses Feito!

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -30,18 +30,18 @@ function Cadastro() {
       <div className="image-container">
         <img id="logo-principal" src={calender} alt="Finito" />
         <div className="mes-overlay">
-          <h1 onClick={()=> alert('O ano escolhido foi ' + anoSelecionado)} className="JANEIRO">JANEIRO</h1>
-          <h1 className="FEVEREIRO">FEVEREIRO</h1>
-          <h1 className="MARCO">MARCO</h1>
-          <h1 className="ABRIL">ABRIL</h1>
-          <h1 className="MAIO">MAIO</h1>
-          <h1 className="JUNHO">JUNHO</h1>
-          <h1 className="JULHO">JULHO</h1>
-          <h1 className="AGOSTO">AGOSTO</h1>
-          <h1 className="SETEMBRO">SETEMBRO</h1>
-          <h1 className="OUTUBRO">OUTUBRO</h1>
-          <h1 className="NOVEMBRO">NOVEMBRO</h1>
-          <h1 className="DEZEMBRO">DEZEMBRO</h1>
+          <h1 onClick={() => alert('Janeiro de ' + anoSelecionado)} className="JANEIRO">JANEIRO</h1>
+          <h1 onClick={() => alert('Fevereiro de ' + anoSelecionado)} className="FEVEREIRO">FEVEREIRO</h1>
+          <h1 onClick={() => alert('Março de ' + anoSelecionado)} className="MARCO">MARÇO</h1>
+          <h1 onClick={() => alert('Abril de ' + anoSelecionado)} className="ABRIL">ABRIL</h1>
+          <h1 onClick={() => alert('Maio de ' + anoSelecionado)} className="MAIO">MAIO</h1>
+          <h1 onClick={() => alert('Junho de ' + anoSelecionado)} className="JUNHO">JUNHO</h1>
+          <h1 onClick={() => alert('Julho de ' + anoSelecionado)} className="JULHO">JULHO</h1>
+          <h1 onClick={() => alert('Agosto de ' + anoSelecionado)} className="AGOSTO">AGOSTO</h1>
+          <h1 onClick={() => alert('Setembro de ' + anoSelecionado)} className="SETEMBRO">SETEMBRO</h1>
+          <h1 onClick={() => alert('Outubro de ' + anoSelecionado)} className="OUTUBRO">OUTUBRO</h1>
+          <h1 onClick={() => alert('Novembro de ' + anoSelecionado)} className="NOVEMBRO">NOVEMBRO</h1>
+          <h1 onClick={() => alert('Dezembro de ' + anoSelecionado)} className="DEZEMBRO">DEZEMBRO</h1>
         </div>
       </div>
     </div>
