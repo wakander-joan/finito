3a30df04147185b4a5e557f3a9bf70f4f3309e0f
joan.santos <joansantosreis@gmail.com>
2025-09-03 17:58:28 -0300

    Cabeçalho modularizado!

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,11 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './styledash.css';
 import api from '../../services/api';
-
-{/* Import's Cabecalho.............*/ }
-import Logo from '../../assets/logo.png';
-import Exit from '../../assets/back.png';
-import loadingGif4 from '../../assets/loading4.gif';
+import Cabecalho from './cabecalho'
 
 {/* Import's Input's...............*/ }
 
@@ -25,6 +21,7 @@ import carteiraicon from '../../assets/carteiraicon.png';
 
 
 function Dashboard() {
+  const [loading, setLoading] = useState(false);
 
   {/* Body-response-Geral.................................................................*/ }
   {/* Body iniciado antes das constantes para carregamento dos, VALORES & LANCAMENTOS.....*/ }
@@ -68,30 +65,7 @@ function Dashboard() {
   };
 
 
-  {/* Const's Cabecalho.................*/ }
-  const nomePessoa = localStorage.getItem("nomePessoa");
-  const [loading, setLoading] = useState(false);
-  const setlaD = "--->"
-  const setlaE = "<---"
-  const EMOJIS = [
-    "🕎", // 0
-    "💀", // 1
-    "💸", // 2
-    "😬", // 3
-    "😐", // 4
-    "🙂", // 5
-    "😎", // 6
-    "🤑", // 7
-    "💰", // 8
-    "🏦", // 9
-    "🌟"  // 10
-  ];
-  const meses = [
-    "JANEIRO", "FEVEREIRO", "MARCO", "ABRIL",
-    "MAIO", "JUNHO", "JULHO", "AGOSTO",
-    "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"
-  ];
-  const perfilEmoji = getPerfilEmoji();
+  
 
   {/* Const's Valores.................*/ }
   const totalReceitas = somarReceitas(body_response);
@@ -147,52 +121,6 @@ function Dashboard() {
 
   {/* Inicio das Functions ----------------------------------- */ }
 
-
-  {/* Cabecalhooooooooooooooooooooooo.................*/ }
-  function getPerfilEmoji() {
-    const perfil = localStorage.getItem("perfil"); // Ex.: "3"
-    const perfilIndex = parseInt(perfil, 10); // Converte para número
-
-    if (!isNaN(perfilIndex) && perfilIndex >= 0 && perfilIndex < EMOJIS.length) {
-      return EMOJIS[perfilIndex];
-    }
-    return "❓"; // Caso não exista
-  }
-
-  async function voltar_menu_animacao() {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      navigate('/cadastro')
-    }, 1700);
-  }
-
-  async function get_next(mes) {
-    const response = await api.get(`/lancamento/buscaLancamentosPorMesEAno/${mes}/${anoSelecionado}`);
-    console.log('Resultado:', response);
-    localStorage.setItem('body-response-array', JSON.stringify(response.data))
-    localStorage.setItem('mes-selecionado', mes)
-    localStorage.setItem('ano-selecionado', anoSelecionado)
-    navigate('/dashboard')
-    return;
-  }
-
-  function direcaoSetaClicada(direcao) {
-    // Pega o índice do mês atualmente selecionado
-    const indiceAtual = meses.indexOf(messelecionado); // mesSelecionado deve estar no estado
-
-    let novoIndice;
-    if (direcao === "proximo") {
-      // próximo mês (loop no final)
-      novoIndice = (indiceAtual + 1) % meses.length;
-    } else if (direcao === "anterior") {
-      // mês anterior (loop no começo)
-      novoIndice = (indiceAtual - 1 + meses.length) % meses.length;
-    }
-
-    const novoMes = meses[novoIndice];
-    get_next(novoMes); // chama sua função
-  }
 
   {/* Valoressssssssssssss.................*/ }
   function somarReceitasFormatadas(bodyArray) {
@@ -389,27 +317,7 @@ function Dashboard() {
           <p class="typng"><span class="dotes"></span></p>
         </div>
       )}
-      <div className='Cabecalho'>
-        <div className='Areadash1'>
-          <img onClick={voltar_menu_animacao} id="logo-exit-dash" src={Exit} alt="Finito" />
-          <img id="logo-finito-cabecalho-dash" src={Logo} alt="Finito" />
-          <h2 id='FINITO-TEXT-DASH'>FINITO</h2>
-        </div>
-
-        <div className='Areadash2'>
-          <h5 id='seta' onClick={() => direcaoSetaClicada("anterior")}>{setlaE}</h5>
-          <h5 id='ano-cabecalho'>{messelecionado}</h5>
-          <h5 id='de-cabecalho'>de</h5>
-          <h5 id='ano-cabecalho'>{anoSelecionado}</h5>
-          <h5 id='seta' onClick={() => direcaoSetaClicada("proximo")}>{setlaD}</h5>
-        </div>
-
-        <div className='Area3'>
-          <h2 id='USUARIO-TEXT2'>{nomePessoa}</h2>
-          <h2 id='PERFIL-EMOGI2'>{perfilEmoji}</h2>
-        </div>
-      </div>
-
+      <Cabecalho />
       {/* Dashboard Principal */}
       <div className='Caixa-dados'>
         {/* Valores de amostragem */}
@@ -743,6 +651,7 @@ function Dashboard() {
           </div>
           {/* Final div Lancamentos*/}
         </div>
+
       </div>
 
     </div>
