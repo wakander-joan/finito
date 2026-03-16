a4a984b0203a660006293d21c54c13e8e2671c8c
joan.santos <joansantosreis@gmail.com>
2026-03-16 16:48:02 -0300

    Responsividade do Metas

--- src/pages/Graficos/index.jsx
+++ src/pages/Graficos/index.jsx
@@ -1,47 +1,22 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import api from "../../services/api";
 import "./PlanosPage.css";
 import loadingGif2 from '../../assets/loading5.gif';
 import goku from '../../assets/loading3 - Copia.gif';
-import Exit from '../../assets/back.gif';
 import { useNavigate } from 'react-router-dom';
 import add from '../../assets/add.png';
 import apaga from '../../assets/apaga.png';
 
 export default function PlanosPage() {
-  // exemplo de dado vindo da API: "13/30"
-  function calculaProgresso(totalEtapasConcluidas, totalEtapas) {
-    if (!totalEtapas || totalEtapas === 0) return 0; // evita divisão por zero
-
-    const percentage = Math.round(
-      (totalEtapasConcluidas / totalEtapas) * 100
-    );
-
-    return percentage;
-  }
-
-
   const [loading, setLoading] = useState(false);
-  const anoSelecionado = localStorage.getItem("ano-selecionado");
   const navigate = useNavigate();
   const [showForm, setShowForm] = useState(false);
-
-  // Constantes para armazenar os dados do formulário
   const [description, setDescription] = useState("");
-  const [valueStart, setValueStart] = useState("");
-  const [valueFinish, setValueFinish] = useState("");
-
   const [dataInicial, setDataInicio] = useState("");
   const [dataAlvo, setDataAlvo] = useState("");
-  const idPessoa = localStorage.getItem("idPessoa");
-  const [planoJsonText, setPlanoJson] = useState();
-  const [idMetaCriada, setIdMetaCriada] = useState("");
-
   const [anotacaoMeta, setAnotacaoMeta] = useState("");
   const [totalEtapas, setTotalEtapas] = useState(0);
-
   const [listaEtapas, setListaEtapas] = useState([]);
-
   const [descEtapaForm, setDescEtapaForm] = useState(false);
   const [anotacaoEtapa, setAnotacaoEtapa] = useState("");
   const [descricaoEtapa, setDescricaoEtapa] = useState("");
@@ -54,25 +29,23 @@ export default function PlanosPage() {
     CONCLUIDA: "#00ad2e"
   };
 
-  const idUsuario = idPessoa;
-
-
-  async function voltar_menu() {
-    navigate('/cadastro')
-  }
-
   const audioClick = new Audio("/click.mp3");
   const audioclickGTA = new Audio("/clickGTA.mp3");
   const audioPassedGTA5 = new Audio("/passedGTA5.mp3");
   const audioAtualiza = new Audio("/atualiza.mp3");
+  const clickGTA = new Audio("/clickGTA.mp3");
 
   const tocarSom = (som) => {
-    som.currentTime = 0; // reinicia o áudio do começo
+    som.currentTime = 0;
     som.play();
   };
 
+  async function voltar_menu() {
+    navigate('/cadastro');
+  }
+
   async function CadastraMeta(bodyJson) {
-    alert("Entrou no cadastra Meta!")
+    alert("Entrou no cadastra Meta!");
     const response = await api.post(`/meta/cadastraMeta`, bodyJson);
     if (response.status === 403) {
       alert('⚠ Você precisa fazer login novamente!');
@@ -80,60 +53,41 @@ export default function PlanosPage() {
       navigate('/');
     }
     if (response.status === 201) {
-      alert("Meta cadastrada com sucesso!")
-      //const response = await api.get(`/lancamento/buscaLancamentosPorMesEAno/${messelecionado}/${anoSelecionado}`);
-      if (response.status === 403) {
-        alert('⚠ Você precisa fazer login novamente!');
-        localStorage.removeItem('token');
-        navigate('/');
-      }
-      console.log('Resultado:', response);
-      //localStorage.setItem('body-response-array', JSON.stringify(response.data))
-      window.location.reload()
+      alert("Meta cadastrada com sucesso!");
+      window.location.reload();
       setShowForm(false);
-      return;
     } else {
       alert(`⚠ Algo deu errado! Código: ${response.status}`);
-      console.log('Algo deu errado!', response);
     }
   }
 
-  //Função que busca as metas
   const carregarMetas = async () => {
     const response = await api.get(`/meta/buscaMetas`);
-
     if (response.status === 403) {
       alert('⚠ Você precisa fazer login novamente!');
       localStorage.removeItem('token');
       navigate('/');
     }
     if (response.status === 200) {
-      localStorage.setItem('body-metas-array', JSON.stringify(response.data))
-      console.log('Resposta: ', response.data);
+      localStorage.setItem('body-metas-array', JSON.stringify(response.data));
     } else {
       alert(`⚠ Algo deu errado! Código: ${response.status}`);
-      console.log('Algo deu errado!', response);
     }
   };
 
   useEffect(() => {
     const metasString = localStorage.getItem('body-metas-array');
     if (metasString) {
-      const metasParsed = JSON.parse(metasString);
-      console.log("metasParsed:", metasParsed);
-      setMetas(metasParsed);
+      setMetas(JSON.parse(metasString));
     }
   }, []);
 
-
   useEffect(() => {
     carregarMetas();
-  }, []); // [] significa "executa só uma vez, quando o componente monta"
-
+  }, []);
 
   async function alteraStatusEtapa(status, idEtapa) {
-    //alert(`Entrou no alteraStatusEtapa! ${status}, ${idEtapa}`);
-    setLoading(true)
+    setLoading(true);
     const response = await api.patch(`/meta/alteraStatusEtapa/${idEtapa}/${status}`);
     if (response.status === 403) {
       alert('⚠ Você precisa fazer login novamente!');
@@ -141,27 +95,21 @@ export default function PlanosPage() {
       navigate('/');
     }
     if (response.status === 204) {
-      //tocarSom(audioPassedGTA5)
-      console.log('Resultado:', response);
-      //alert('Alteração realizada!');
       carregarMetas();
       setTimeout(() => {
-        setLoading(false)
-        tocarSom(audioAtualiza)
-        setTimeout(() => {
-          window.location.reload();
-        }, 500);
+        setLoading(false);
+        tocarSom(audioAtualiza);
+        setTimeout(() => { window.location.reload(); }, 500);
       }, 1500);
     } else {
       alert(`⚠ Algo deu errado! Código: ${response.status}`);
-      console.log('Algo deu errado!', response);
     }
   }
 
   return (
     <div className="page-container">
       <div className="cabecalho-planing">
-        <p className="page-title">  METAS🪙  </p>
+        <p className="page-title">METAS🪙</p>
         <button className="criar" id="criar" onClick={() => { setShowForm(true); tocarSom(audioclickGTA); }} title="Criar nova meta!">NOVA META</button>
         <button className="criar" id="sair" onClick={() => { voltar_menu(); tocarSom(audioClick); }} title="Sair">🔙</button>
       </div>
@@ -172,12 +120,10 @@ export default function PlanosPage() {
         </div>
       )}
 
-      {/* Overlay Cria Meta*/}
       {showForm && (
         <div className="overlay">
           <div className="modalMeta">
             <h2 className="Titulo-New">Nova Meta🚀</h2>
-
             <form id="div-input" className="div-123">
               <label id="label-meta" htmlFor="text">Descrição</label>
               <input
@@ -204,7 +150,6 @@ export default function PlanosPage() {
                 <input
                   onClick={() => tocarSom(audioclickGTA)}
                   type="date"
-                  placeholder="Data de Vencimento (dd-mm-aaaa)"
                   value={dataInicial}
                   onChange={(e) => setDataInicio(e.target.value)}
                   id="data-plano"
@@ -217,7 +162,6 @@ export default function PlanosPage() {
                 <input
                   onClick={() => tocarSom(audioclickGTA)}
                   type="date"
-                  placeholder="Data de Vencimento (dd-mm-aaaa)"
                   value={dataAlvo}
                   onChange={(e) => setDataAlvo(e.target.value)}
                   id="data-plano"
@@ -227,11 +171,7 @@ export default function PlanosPage() {
 
               <div className="add-div">
                 <img id='add' src={add} className="icon"
-                  onClick={() => {
-                    setDescEtapaForm(true);
-                    //alert(`Adicionando mais uma etapa... \n Total de etapas ${totalEtapas + 1}`);
-                    tocarSom(audioClick);
-                  }} />
+                  onClick={() => { setDescEtapaForm(true); tocarSom(audioClick); }} />
                 <label id="Label-nova-etapa" htmlFor="text">Adicionar nova etapa...</label>
               </div>
 
@@ -242,11 +182,8 @@ export default function PlanosPage() {
                     <input
                       type="text"
                       placeholder="Descrição etapa"
-                      onChange={(e) =>
-                        setDescricaoEtapa(e.target.value)
-                      }
+                      onChange={(e) => setDescricaoEtapa(e.target.value)}
                     />
-
                     <label id="label-meta" htmlFor="text">Anotação</label>
                     <textarea
                       id="anotacao-meta"
@@ -255,64 +192,35 @@ export default function PlanosPage() {
                       onChange={(e) => setAnotacaoEtapa(e.target.value)}
                       placeholder="Adicione uma anotação aqui..."
                     />
-
-                    <button
-                      className="botoes"
-                      id="editar"
-                      onClick={() => {
-                        const proximoNumero = listaEtapas.length + 1; // pega o próximo número da etapa
-                        setListaEtapas(prev => [
-                          ...prev,
-                          { numero: proximoNumero, descricao: descricaoEtapa, anotacao: anotacaoEtapa } // grava número + descrição
-                        ]);
-                        setTotalEtapas(prev => prev + 1); // opcional, se você quiser atualizar totalEtapas
-                        setDescricaoEtapa(''); // limpa o input
-                        setAnotacaoEtapa(''); // limpa o input
-                        setDescEtapaForm(false)
-                      }}
-                    >
-                      Gravar
-                    </button>
-
-                    <button
-                      className="botoes"
-                      id="editar"
-                      onClick={() => { setDescEtapaForm(false) }}
-                    >
-                      Cancelar
-                    </button>
+                    <button className="botoes" id="editar" onClick={() => {
+                      setListaEtapas(prev => [...prev, { numero: prev.length + 1, descricao: descricaoEtapa, anotacao: anotacaoEtapa }]);
+                      setTotalEtapas(prev => prev + 1);
+                      setDescricaoEtapa('');
+                      setAnotacaoEtapa('');
+                      setDescEtapaForm(false);
+                    }}>Gravar</button>
+                    <button className="botoes" id="editar" onClick={() => setDescEtapaForm(false)}>Cancelar</button>
                   </div>
                 </div>
               )}
 
-              {/* Aqui ele exibe a lista de Etapas...................................................................*/}
               {listaEtapas.map((etapa, index) => (
                 <div className="etapa" key={index}>
                   <div id="etapa-subdiv">
-
-
-                    <label id="descricao-etapa">Etapa {index + 1}: {etapa.descricao}</label> <br />
-
+                    <label id="descricao-etapa">Etapa {index + 1}: {etapa.descricao}</label>
                     <div>
-                      <img
-                        id="del"
-                        src={apaga}
-                        className="icon"
+                      <img id="del" src={apaga} className="icon"
                         onClick={() => {
                           tocarSom(audioclickGTA);
-
                           setTotalEtapas(prev => prev - 1);
-
-                          // Remove a etapa clicada e reorganiza números
                           setListaEtapas(prev => {
                             const novaLista = [...prev];
-                            novaLista.splice(index, 1); // remove a etapa
-                            return novaLista; // indices vão servir como novo número
+                            novaLista.splice(index, 1);
+                            return novaLista;
                           });
                         }}
                       />
                     </div>
-
                   </div>
                   <textarea
                     id="anotacao-textarea"
@@ -323,32 +231,10 @@ export default function PlanosPage() {
                 </div>
               ))}
 
-
               <div className="div-botoes">
-                <button
-                  className="botoes"
-                  type="button"
-                  id="editar"
-                  onClick={() => { // Mostrar todas as etapas
+                <button className="botoes" type="button" id="editar"
+                  onClick={() => {
                     if (listaEtapas && listaEtapas.length > 0) {
-                      alert(
-                        JSON.stringify(
-                          {
-                            descricao: description,
-                            anotacao: anotacaoMeta,
-                            dataInicial: dataInicial,
-                            dataAlvo: dataAlvo,
-                            etapas: listaEtapas.map((etapa, index) => ({
-                              numero: index + 1,
-                              descricao: etapa.descricao,
-                              anotacao: etapa.anotacao
-                            }))
-                          },
-                          null,
-                          2 // identação de 2 espaços
-                        )
-                      );
-
                       const bodyJson = {
                         descricao: description,
                         anotacao: anotacaoMeta,
@@ -360,48 +246,30 @@ export default function PlanosPage() {
                           anotacao: etapa.anotacao
                         }))
                       };
-
                       CadastraMeta(bodyJson);
                     } else {
-                      alert("⚠️ A meta deve ter no minimo uma etapa!")
+                      alert("⚠️ A meta deve ter no mínimo uma etapa!");
                     }
-
                   }}
-                >
-                  Enviar
-                </button>
-
-                <button
-                  className="botoes"
-                  type="button"
-                  onClick={() => { window.location.reload() }}
-                  id="excluir"
-                >
-                  Cancelar
-                </button>
+                >Enviar</button>
+                <button className="botoes" type="button" onClick={() => window.location.reload()} id="excluir">Cancelar</button>
               </div>
             </form>
           </div>
         </div>
       )}
-      {/* fin da Overlayer */}
 
-      {/*Começa overlayer de Metas.............................*/}
       {metas.length === 0 ? (
         <p className="text-center">Nenhuma meta foi encontrada...</p>
       ) : (
         <div className="planos-grid">
           {metas.map((metaBuscada) => {
-
-            const porcentagem =
-              metaBuscada.totalEtapas > 0
-                ? Math.round(
-                  (metaBuscada.totalEtapasConcluidas / metaBuscada.totalEtapas) * 100
-                )
-                : 0;
+            const porcentagem = metaBuscada.totalEtapas > 0
+              ? Math.round((metaBuscada.totalEtapasConcluidas / metaBuscada.totalEtapas) * 100)
+              : 0;
 
             return (
-              <div className="div-metas" key={metaBuscada.idMeta} >
+              <div className="div-metas" key={metaBuscada.idMeta}>
                 <div className="modalMetaBuscada">
                   <div className="texto-caixa">
                     <h2 id="label-meta-map">Descrição Meta:</h2>
@@ -410,10 +278,7 @@ export default function PlanosPage() {
                   <p>Anotação Meta: {metaBuscada.anotacao}</p>
 
                   <div className="progress-container">
-                    <div
-                      className="progress-bar"
-                      style={{ width: `${porcentagem}%` }}
-                    />
+                    <div className="progress-bar" style={{ width: `${porcentagem}%` }} />
                     <img id='img-gokui-barra' src={goku} className="icon" />
                     <p className="progress-text">{porcentagem}%</p>
                   </div>
@@ -423,10 +288,10 @@ export default function PlanosPage() {
                     .sort((a, b) => a.numero - b.numero)
                     .map((etapa) => (
                       <div className="etapa-div" key={etapa.idEtapa}>
-                        <p >Etapa {etapa.numero}</p>
+                        <p>Etapa {etapa.numero}</p>
                         <p>Descrição etapa: {etapa.descricao}</p>
                         <p>Anotação etapa: {etapa.anotacao}</p>
-                        <div className=''>
+                        <div>
                           <p>Status</p>
                           <select
                             className="Status-Select"
@@ -434,11 +299,8 @@ export default function PlanosPage() {
                             style={{ color: statusColors[etapa.status] || "#00cbfd" }}
                             value={etapa.status}
                             onChange={(e) => {
-                              const statusString = e.target.value
-                                .replace(/\s/g, "_")
-                                .toUpperCase();
+                              const statusString = e.target.value.replace(/\s/g, "_").toUpperCase();
                               tocarSom(audioPassedGTA5);
-
                               alteraStatusEtapa(statusString, etapa.idEtapa);
                             }}
                             onClick={() => tocarSom(clickGTA)}
